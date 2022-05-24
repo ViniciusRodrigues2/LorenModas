34470b18d88db83f24f718816d67fa1dec076aac
Code-Space <100551018+ViniciusRodrigues2@users.noreply.github.com>
2022-05-24 01:29:58 -0300

    commit inicial

---
+++ js/mudarcamisa.js
@@ -0,0 +1,18 @@
+var mudarCor = document.querySelector('.mudarColor');
+var mudarBlack = document.querySelector('.mudarBlack');
+function mudar(e){
+    var img = document.querySelector('.img-ctn');
+    if(e.target.className == 'mudarColor')
+    {
+        img.src = '../image/white-t-shirt-mockup.webp'
+    }
+    function mudarB (e){
+        var img = document.querySelector('.img-ctn');
+        if(e.target.className == 'mudarBlack'){
+            img.src = '../image/black-t-shirt-mockup.webp'
+        }
+    }
+    mudarBlack.addEventListener('click',mudarB)
+}
+
+mudarCor.addEventListener('click',mudar);
